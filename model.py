fix the dirty bug in precision and recall calculation

model.py
@@ -4,6 +4,7 @@ import torch.nn as nn
 from data_utils import START_TAG_IDX, STOP_TAG_IDX, PAD_IDX
 
 EMBED_SIZE = 300
+CHAR_EMBEDDING_DIM = 100
 HIDDEN_SIZE = 300
 NUM_LAYERS = 2
 DROPOUT = 0.5
@@ -17,44 +18,84 @@ torch.manual_seed(1)
 CUDA = torch.cuda.is_available()
 
 
+def _sort(_2dtensor, lengths, descending=True):
+    sorted_lengths, order = lengths.sort(descending=descending)
+    _2dtensor_sorted_by_lengths = _2dtensor[order]
+    return _2dtensor_sorted_by_lengths, order
+
+
 class lstm_crf(nn.Module):
-    def __init__(self, vocab_size, num_tags, embeddings):
+    def __init__(
+            self,
+            vocab_size,
+            num_tags,
+            embeddings,
+            num_char_embeddings,
+            char_lstm):
         super(lstm_crf, self).__init__()
-        self.lstm = lstm(vocab_size, num_tags, embeddings)
+        self.lstm = lstm(
+            vocab_size,
+            num_tags,
+            embeddings,
+            num_char_embeddings=num_char_embeddings,
+            char_embedding_dim=CHAR_EMBEDDING_DIM,
+            char_lstm=char_lstm)
+
         self.crf = crf(num_tags)
         self = self.cuda() if CUDA else self
 
-    def forward(self, x, y):  # for training
-        mask = x.data.gt(0).float()  # because 0 is pad_idx, doesn't really belong here, I guess
-        h = self.lstm(x, mask)
+    def forward(self, word_x, char_x, y):  # for training
+        mask = word_x.data.gt(0).float()  # because 0 is pad_idx, doesn't really belong here, I guess
+        h = self.lstm(word_x, mask, char_x)
         Z = self.crf.forward(h, mask)  # partition function
         score = self.crf.score(h, y, mask)
         return Z - score  # NLL loss
 
-    def decode(self, x):  # for prediction
-        mask = x.data.gt(0).float()  # again 0 is probably because of pad_idx, pass mask as parameter
-        h = self.lstm(x, mask)
+    #TODO : add char lstm part
+    def decode(self, word_x, char_x):  # for prediction
+        mask = word_x.data.gt(0).float()  # again 0 is probably because of pad_idx, pass mask as parameter
+        h = self.lstm(word_x, mask, char_x)
         return self.crf.decode(h, mask)
 
 
 # TODO: comment all dimension conversions for better understanding!
 class lstm(nn.Module):
-    def __init__(self, vocab_size, num_tags, embeddings):
+    def __init__(
+            self,
+            vocab_size,
+            num_tags,
+            embeddings,
+            num_char_embeddings,
+            char_embedding_dim,
+            char_lstm,
+            char_padding_idx=0):
         super(lstm, self).__init__()
 
         # architecture
-        self.embed = nn.Embedding(vocab_size, EMBED_SIZE, padding_idx=PAD_IDX, _weight=embeddings)
-        self.embed.weight.requires_grad = False
+        self.char_embeddings = nn.Embedding(
+            num_embeddings=num_char_embeddings,
+            embedding_dim=char_embedding_dim,
+            padding_idx=char_padding_idx)
+
+        self.word_embeddings = nn.Embedding(
+            vocab_size,
+            EMBED_SIZE,
+            padding_idx=PAD_IDX,
+            _weight=embeddings)
+
+        self.word_embeddings.weight.requires_grad = False
+
+        self.char_lstm = char_lstm
 
         self.lstm = nn.LSTM(
-            input_size=EMBED_SIZE,
+            input_size=EMBED_SIZE + 200,
             hidden_size=HIDDEN_SIZE // NUM_DIRS,
             num_layers=NUM_LAYERS,
             bias=True,
             batch_first=True,
             dropout=DROPOUT,
-            bidirectional=BIDIRECTIONAL
-        )
+            bidirectional=BIDIRECTIONAL)
+
         self.out = nn.Linear(HIDDEN_SIZE, num_tags)  # LSTM output to tag
 
     def init_hidden(self, batch_size):  # initialize hidden states
@@ -62,17 +103,45 @@ class lstm(nn.Module):
         c = zeros(NUM_LAYERS * NUM_DIRS, batch_size, HIDDEN_SIZE // NUM_DIRS)  # cell states
         return (h, c)
 
-    def forward(self, x, mask):
-        initial_hidden = self.init_hidden(x.shape[0])  # batch size is first
-        x = self.embed(x)
-        x = nn.utils.rnn.pack_padded_sequence(x, mask.sum(1).int(), batch_first=True)
-        output, hidden = self.lstm(x, initial_hidden)
-        # print(output.size(), hidden[0].size(), hidden[1].size())
-        print(output)
+    def forward(self, word_x, mask, char_x):
+
+        char_output = self._char_forward(char_x)
+        batch_size = word_x.size(0)
+        max_seq_len = word_x.size(1)
+        char_output = char_output.reshape(batch_size, max_seq_len, -1)  # last dimension is for char lstm hidden size
+
+        word_x = self.word_embeddings(word_x)
+
+        word_x = torch.cat([word_x, char_output], -1)
+
+        initial_hidden = self.init_hidden(batch_size)  # batch size is first
+        word_x = nn.utils.rnn.pack_padded_sequence(word_x, mask.sum(1).int(), batch_first=True)
+        output, hidden = self.lstm(word_x, initial_hidden)
 
         output, recovered_lengths = nn.utils.rnn.pad_packed_sequence(output, batch_first=True)
         output = self.out(output)  # batch x seq_len x num_tags
         output *= mask.unsqueeze(-1)  # mask - batch x seq_len -> batch x seq_len x 1
+        return output
+
+    def _char_forward(self, x):
+        word_lengths = x.gt(0).sum(1)  # actual word lengths
+        sorted_padded, order = _sort(x, word_lengths)
+        # print(sorted_padded)
+        embedded = self.char_embeddings(sorted_padded)
+
+        word_lengths_copy = word_lengths.clone()
+        word_lengths_copy[word_lengths == 0] = 1
+        packed = torch.nn.utils.rnn.pack_padded_sequence(embedded, word_lengths_copy[order], True)
+        packed_output, _ = self.char_lstm(packed)
+        output, _ = torch.nn.utils.rnn.pad_packed_sequence(packed_output, True)
+
+        _, reverse_sort_order = torch.sort(order, dim=0)
+        output = output[reverse_sort_order]
+
+        indices_of_lasts = (word_lengths_copy - 1).unsqueeze(1).expand(-1, output.shape[2]).unsqueeze(1)
+        output = output.gather(1, indices_of_lasts).squeeze()
+        output[word_lengths == 0] = 0
+        # return output.reshape(len(batch), max_sentence_length, -1)
         return output
 
 
@@ -176,14 +245,12 @@ class crf(nn.Module):
         return best_path
 
 
-
 def _sort_and_reverse_sorting(_2dtensor, lengths):
     sorted_lengths, order = lengths.sort(descending=True)
     _2dtensor_sorted_by_lengths = _2dtensor[order]
-    #do something and reverse
+    # do something and reverse
     unsorted = _2dtensor_sorted_by_lengths.new(*_2dtensor.size())
     return unsorted.scatter_(0, order.unsqueeze(1).expand(*_2dtensor.size()), _2dtensor_sorted_by_lengths)
-
 
 
 def reverse_sorting_single_vector(_sorted, order):

predict.py
@@ -7,34 +7,51 @@ from utils import *
 
 # TODO: model.train should be called with False before evaluation?
 def _predict():
-    word2idx, tag2idx = _load_words_tags_chars("data/words.txt", "data/tags.txt")
+    # word2idx, tag2idx = _load_words_tags_chars("data/words.txt", "data/tags.txt")
+    word2idx, tag2idx, char2idx = _load_words_tags_chars("data/words.txt", "data/tags.txt", "data/chars.txt")
+
     idx2word = {idx: word for word, idx in word2idx.items()}
     idx2tag = {idx: tag for tag, idx in tag2idx.items()}
     embeddings = load_vocab_vectors("data/filtered_embeddings.txt")
-    model = lstm_crf(len(word2idx), len(tag2idx), torch.tensor(embeddings, dtype=torch.float))
+
+    char_lstm = torch.nn.LSTM(
+        input_size=CHAR_EMBEDDING_DIM,
+        hidden_size=200 // 2,
+        num_layers=1,
+        bias=True,
+        batch_first=True,
+        bidirectional=True)
+
+    model = lstm_crf(
+        len(word2idx), len(tag2idx),
+        torch.tensor(embeddings, dtype=torch.float),
+        len(char2idx) + 1,
+        char_lstm)
+
+    # model = lstm_crf(len(word2idx), len(tag2idx), torch.tensor(embeddings, dtype=torch.float))
     model.eval()
     print(model)
-    load_checkpoint('0.epoch7', model)
+    load_checkpoint('0.epoch6', model)
 
-    arrays = _load_data('data/more_annotated_test.conll', None, word2idx, tag2idx)
+    # arrays = _load_data('data/more_annotated_test.conll', None, word2idx, tag2idx)
+    arrays = _load_data('data/more_annotated_test.conll', word2idx, tag2idx, char2idx)
+
     type2referenced = defaultdict(set)
     type2predicted = defaultdict(set)
 
     sentenceIdx = 0
-    for x, y in batch_generator(*arrays):
-        y_pred = model.decode(x)
+    for word_x, char_x, y in batch_generator(*arrays):
+        y_pred = model.decode(word_x, char_x)
 
-        for xi, yi, yp in zip(x, y, y_pred):
+        for xi, yi, yp in zip(word_x, y, y_pred):
             text, predicted_tags = _instance_from(yp, xi, idx2word, idx2tag)
             _, original_tags = _instance_from(yi, xi, idx2word, idx2tag)
-            length = int(xi.gt(0).sum()) #TODO: replace check with 0 with check for pad index
+            length = int(xi.gt(0).sum())  # TODO: replace check with 0 with check for pad index
             referenced = _get_spans(text[:length], original_tags[:length])
             try:
                 predicted = _get_spans(text[:length], predicted_tags[:length])
 
             except ValueError:
-                print('skipping', text[:length])
-                print(predicted_tags[:length])
                 continue
 
             for reference in referenced:
@@ -47,14 +64,14 @@ def _predict():
 
     print('recalls')
     for type, referenced in type2referenced.items():
-        print(type, len(referenced), len(referenced.difference(type2predicted[type])),
-              float(len(referenced.difference(type2predicted[type]))) / len(referenced))
+        print(type, len(referenced), len(referenced.intersection(type2predicted[type])),
+              float(len(referenced.intersection(type2predicted[type]))) / len(referenced))
 
     print()
     print('precisions')
     for type, predicted in type2predicted.items():
-        print(type, len(predicted), len(predicted.difference(type2referenced[type])),
-              float(len(predicted.difference(type2referenced[type]))) / len(predicted))
+        print(type, len(predicted), len(predicted.intersection(type2referenced[type])),
+              float(len(predicted.intersection(type2referenced[type]))) / len(predicted))
 
 
 def _instance_from(predicted, entry, idx2word, idx2tag):
@@ -66,6 +83,3 @@ def _instance_from(predicted, entry, idx2word, idx2tag):
 if __name__ == "__main__":
     print("cuda: %s" % CUDA)
     _predict()
-
-
-

train.py
@@ -57,6 +57,14 @@ def batch_generator(*arrays, batch_size=32):
         batch_lengths = seq_length_in_words[startIdx:endIdx]
         batch_maxlen = batch_lengths.max()
         argsort = numpy.argsort(batch_lengths)[::-1].copy()  # without the copy torch complains about negative strides
+        char_batch = numpy.array(char_id_lists[startIdx:endIdx])[argsort]
+        char_batch = [sentence + ((0,),) * (batch_maxlen - len(sentence)) for sentence in char_batch]
+
+        word_lengths = [len(word) for sentence in char_batch for word in sentence]
+        max_word_length = max(word_lengths)
+        chars = [word + (0,) * (max_word_length - len(word)) for sentence in char_batch for word in sentence]
+
+        chars = LongTensor(chars)
 
         words = LongTensor([word_ids + (PAD_IDX,) * (batch_maxlen - len(word_ids))
                             for word_ids in word_id_lists[startIdx:endIdx]])
@@ -64,7 +72,7 @@ def batch_generator(*arrays, batch_size=32):
                            for tag_ids in tag_id_lists[startIdx:endIdx]])
 
         prog.update(len(batch_lengths))
-        yield words[argsort], tags[argsort]
+        yield words[argsort], chars, tags[argsort]
     prog.close()
 
 
@@ -75,7 +83,21 @@ def train(word_vocab_size,
           train_data,
           valid_data,
           epochs=20):
-    model = lstm_crf(word_vocab_size, tag_vocab_size, torch.tensor(word_embeddings, dtype=torch.float))
+    char_lstm = torch.nn.LSTM(
+        input_size=CHAR_EMBEDDING_DIM,
+        hidden_size=200 // 2,
+        num_layers=1,
+        bias=True,
+        batch_first=True,
+        bidirectional=True)
+    print('char vocab size', char_vocab_size)
+    model = lstm_crf(
+        word_vocab_size,
+        tag_vocab_size,
+        torch.tensor(word_embeddings, dtype=torch.float),
+        char_vocab_size,
+        char_lstm)
+
     optim = torch.optim.Adam(model.parameters(), lr=LEARNING_RATE, weight_decay=WEIGHT_DECAY)
     # TODO: change naming to something better
     filename = re.sub("\.epoch[0-9]+$", "", '0')
@@ -87,9 +109,9 @@ def train(word_vocab_size,
         timer = time.time()
         batch_count = 0
         model.train()
-        for x, y in batch_generator(*train_data):
+        for word_x, char_x, y in batch_generator(*train_data):
             model.zero_grad()
-            loss = torch.mean(model(x, y))  # forward pass and compute loss
+            loss = torch.mean(model(word_x, char_x, y))  # forward pass and compute loss
             loss.backward()  # compute gradients
             optim.step()  # update parameters
             loss = scalar(loss)
@@ -106,8 +128,8 @@ def train(word_vocab_size,
         batch_count = 0
         model.eval()
         with torch.no_grad():
-            for x, y in batch_generator(*valid_data):
-                loss_sum += scalar(torch.mean(model(x, y)))
+            for word_x, char_x, y in batch_generator(*valid_data):
+                loss_sum += scalar(torch.mean(model(word_x, char_x, y)))
                 batch_count += 1
 
             print('validation loss: {}'.format(loss_sum / batch_count))
@@ -128,7 +150,7 @@ if __name__ == "__main__":
     train(
         len(word2idx),
         len(tag2idx),
-        len(char2idx),
+        len(char2idx) + 1,
         load_vocab_vectors(filtered_embeddings_file),
         train_data,
         valid_data,